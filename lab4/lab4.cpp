88a34b05c48b95ef7f99259797ebb3226230fc75
Ryan Spaulding <rspaulding001@student.butte.edu>
2016-09-07 11:32:48 +0000

    Lab4 Submission

---
+++ lab4/lab4.cpp
@@ -0,0 +1,34 @@
+/*  Created By: Ryan Spaulding
+    Date Created: 8/29/16
+    CSCI20
+    Browne
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+    string wordName;
+    string favoritePie;
+    int userNumber = 0;
+    float userDecimal = 0.0;
+    char specialSymbol = '!' ;
+ 
+//inputs    
+    cout << "Enter your name: ";
+    getline (cin, wordName);
+    cout << "Enter your favorite number: ";
+    cin  >> userNumber;
+    cout << "Enter a decimal: ";
+    cin  >> userDecimal;
+    cout << "Enter an exclamation point: ";
+    cin  >> specialSymbol;
+    
+//output
+    cout << "Hello " << wordName << specialSymbol << endl;
+    cout << userNumber << " is a great number." << endl;
+    cout << "However, I only have about " << userDecimal << " of my patience left" << specialSymbol << endl;
+    
+
+}
