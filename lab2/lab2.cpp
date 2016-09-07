bc596e50175aa9ee82bfa0ccd71ec56ff2e61429
Ryan Spaulding <rspaulding001@student.butte.edu>
2016-09-07 10:54:06 +0000

    lab2

---
+++ lab2/lab2.cpp
@@ -0,0 +1,18 @@
+/*Created By: Ryan Spaulding
+Date Created: 8/29/16
+CSI20
+Browne */
+
+/*computer guesses random number between 1 and 10 (n)
+player receives 10 points
+
+player enters number (x)
+if number is correct
+    then player gets -0 points
+    return 6 
+    
+if player is incorrect
+    then player gets -1 points
+    if x > n then cout << "lower.";
+    if x < n then cout << "highter";
+    player guesses again
