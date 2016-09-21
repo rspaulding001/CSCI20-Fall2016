e2b3114ab6ad090d1dc09e0530844e0492668432
Ryan Spaulding <rspaulding001@student.butte.edu>
2016-09-21 06:49:43 +0000

    Lab7 Submission

---
+++ lab7/lab7.cpp
@@ -0,0 +1,85 @@
+/*Created By: Ryan Spaulding
+Date Created: 9/19/16
+CSI20
+Browne */
+
+#include <iostream>
+using namespace std;
+
+void chorusCall()
+{
+
+    cout << "I get knocked down, but I get up again" << endl;
+    cout << "You are never gonna keep me down"       << endl;   
+    cout << "I get knocked down, but I get up again" << endl;
+    cout << "You are never gonna keep me down"       << endl;    
+    cout << "I get knocked down, but I get up again" << endl;
+    cout << "You are never gonna keep me down"       << endl;
+
+
+   return;
+}
+
+void verseCall()
+{
+    
+    cout << "He drinks a Whiskey drink, he drinks a Vodka drink" << endl;
+    cout << "He drinks a Lager drink, he drinks a Cider drink"   << endl;
+    cout << "He sings the songs that remind him of the good times" << endl;
+    cout << "He sings the songs that remind him of the best times" << endl;
+    cout << "(Oh Danny Boy, Danny Boy, Danny Boy)" << endl;
+
+    return;
+}
+    
+int main ()
+{
+    
+    chorusCall();
+    verseCall();
+    chorusCall();
+    verseCall();
+
+return 0;
+}
+
+
+
+
+/* Tubthumping lyrics to be converted to functions
+
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+He drinks a Whiskey drink, he drinks a Vodka drink
+He drinks a Lager drink, he drinks a Cider drink
+He sings the songs that remind him of the good times
+He sings the songs that remind him of the best times
+(Oh Danny Boy, Danny Boy, Danny Boy)
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+He drinks a Whiskey drink, he drinks a Vodka drink
+He drinks a Lager drink, he drinks a Cider drink
+He sings the songs that remind him of the good times
+He sings the songs that remind him of the best times
+(Don't cry for me, next door neighbour)
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+I get knocked down, but I get up again
+You are never gonna keep me down
+*/
